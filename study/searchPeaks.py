0a36a8dd3105f0568faa621c69e243dec7564e55
Andrei Mironov <andrei.mironov@corp.mail.ru>
2020-10-08 15:28:19 +0300

    added vertax traversal

--- study/searchPeaks.py
+++ study/searchPeaks.py
@@ -14,4 +14,27 @@ def search_peaks(i: int, graph_matrix):
             adjacent_peaks.append(k)
     return adjacent_peaks
 
+def bigSearch(start: int, graph_matrix):
+    visited = []
+    adjacent_peaks = []
+    for i in range(len(graph_matrix)):
+        visited.append(False)
+    next = start
+    while False in visited:
+        adjacent_peaks = search_peaks(next, graph_matrix)
+        if len(adjacent_peaks) > 1:
+            for peak in adjacent_peaks:
+                if not visited[peak]:
+                   visited.pop(peak)
+                   visited.insert(peak, True)
+                   print(peak, "is visited")
+        else:
+            if not visited[adjacent_peaks[0]]:
+                visited.pop(adjacent_peaks[0])
+                visited.insert(adjacent_peaks[0], True)
+        next += 1
+    return visited
+
+
 print(search_peaks(3, graph_matrix))
+print(bigSearch(0, graph_matrix))
